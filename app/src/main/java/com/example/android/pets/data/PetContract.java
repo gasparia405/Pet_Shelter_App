d8e34b8af4f3b9a6cef4862e0935d2a1601f6b8b
Andrew Gaspari <gasparia405@gmail.com>
2018-11-12 19:23:11 -0500

    Build URIs to pass into ContentProvider

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -1,12 +1,24 @@
 package com.example.android.pets.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public final class PetContract {
 
     public PetContract(){}
 
+    // Identify Content Authority
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+
+    // Create Base Content Uri
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public final static String PATH_PETS = "pets";
+
     public static final class PetEntry implements BaseColumns {
+
+        // Create complete Content Uri
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
 
         // Table name
         public final static String TABLE_NAME = "pets";
